67f60eae2882f2970dae7e889dbc7827d0e02d7b
Rijul1048 <rijul.ahuja.vishal@gmail.com>
2026-07-12 13:57:52 +0530

    finance route added

--- backend/core/api.py
+++ backend/core/api.py
@@ -5,6 +5,7 @@ from .routers.trips import router as trips_router
 from .routers.maintenance import router as maintenance_router
 from .routers.finance import router as finance_router
 
+
 api = NinjaAPI(
     title="TransitOps API",
     version="1.0.0",
@@ -15,4 +16,4 @@ api.add_router("/vehicles", vehicles_router)
 api.add_router("/drivers", drivers_router)
 api.add_router("/trips", trips_router)
 api.add_router("/maintenance", maintenance_router)
-api.add_router("/finance", finance_router)
+api.add_router("", finance_router)
